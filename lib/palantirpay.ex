2702c65144de8e67fcfb2f3338f56a50ca2f7022
Leo Franco <leofranco1305@gmail.com>
2021-03-18 19:37:23 -0300

    Created deposit feature

--- lib/palantirpay.ex
+++ lib/palantirpay.ex
@@ -1,5 +1,8 @@
 defmodule Palantirpay do
   alias Palantirpay.Users.Create, as: UserCreate
+  alias Palantirpay.Accounts.Deposit
 
   defdelegate create_user(params), to: UserCreate, as: :call
+
+  defdelegate deposit(params), to: Deposit, as: :call
 end
---
+++ lib/palantirpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,16 @@
+defmodule PalantirpayWeb.AccountsController do
+  use PalantirpayWeb, :controller
+
+  alias Palantirpay.Account
+
+  action_fallback PalantirpayWeb.FallbackController
+
+  def deposit(conn, params) do
+    with {:ok, %Account{} = account} <- Palantirpay.deposit(params) do
+      conn
+      |> put_status(:ok)
+      |> render("update.json", account: account)
+
+    end
+  end
+end
